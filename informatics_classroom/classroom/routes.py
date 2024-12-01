4015a3eefb45d60260bb3c9f41dfe3c1b9da8ff1
Robert Barrett <r.barrett1501@gmail.com>
2024-12-01 14:01:26 -0500

    post-auth update implementation

--- informatics_classroom/classroom/routes.py
+++ informatics_classroom/classroom/routes.py
@@ -138,24 +138,8 @@ def submit_answer():
 def assignment(class_val, module):
     """Assignment home"""
 
-    """
-    if not session.get("user"):
-        #Test if user session is set
-        session["return_to"]="classroom_bp.assignment"
-        session['exercise']=f"{class_val}_{module}"
-        return redirect(url_for("auth_bp.login" ))
-    if not session['user'].get('preferred_username').split('@')[1][:2]==Keys.auth_domain:
-        #Test if authenticated user is coming from an authorized domain
-        return redirect(url_for("auth_bp.login"))
-    if len(exercise)==0:
-        if 'return_to' in session.keys():
-            exercise=session['exercise']
+    user_name = ich.check_user_session(session)
 
-    user_name=session['user'].get('preferred_username').split('@')[0]
-    """
-
-    user_name = 'rbarre16'
-    session['user'] = 'rbarre16'
     container=init_cosmos('answer',DATABASE)
     #Query quizes in cosmosdb to get the structure for this assignment
     class_val = escape(class_val)
@@ -232,32 +216,31 @@ def assignment(class_val, module):
 @classroom_bp.route("/exercise_review/<exercise>")
 def exercise_review(exercise):
     """Exercise Review shows all the students and their progress on an Exercise"""
-    if not session.get("user"):
-        #Test if user session is set
-        return redirect(url_for("auth_bp.login"))
-    if not session['user'].get('preferred_username').split('@')[1][:2]==Keys.auth_domain:
-        #Test if authenticated user is coming from an authorized domain
-        return redirect(url_for("auth_bp.login"))
-    #Test if user is an authorized user
-    user_name=session['user'].get('preferred_username').split('@')[0]
+    
+    user_name = ich.check_user_session(session)
+
     course_name=str(exercise).split('_')[0]   
-    authorized_user=False
-    container=init_cosmos('quiz',DATABASE)
-    items=container.read_item(item="auth_users",partition_key="auth")
-    for name in items['users']:
-        if user_name in name:
-        # Test if user is in list of authorized users
-            if course_name in name[user_name]:
-                authorized_user=True
-    if not authorized_user:
-        return redirect(url_for("auth_bp.login"))      
+
+    if not ich.check_authorized_user(session, course_name):
+        return redirect(url_for("auth_bp.login")) 
     # Step 2 get the exercise Structure
      
     # Step 2 get the exercise Structure
     container=init_cosmos('answer',DATABASE)
     #Query quizes in cosmosdb to get the structure for this assignment
     # TODO rbb need to update to wrap queries in something where redirects on bad query
-    query = "SELECT c.PartitionKey, c.course, c.module, c.answer, c.team, c.question, c.correct FROM c where c.PartitionKey = @id"
+    query = """
+    SELECT 
+        c.PartitionKey, 
+        c.course, 
+        c.module, 
+        c.answer, 
+        c.team, 
+        c.question, 
+        c.correct 
+    FROM c 
+    where c.PartitionKey = @id
+    """
 
     # rbb 08/13 - update to parameterized queries
     parameters = [
