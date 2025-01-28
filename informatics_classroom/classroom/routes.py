af2a8aaeff720daef58cfcdf01f2b2e2acc74468
Robert Barrett <r.barrett1501@gmail.com>
2025-01-28 15:40:49 -0500

    remove db reference

--- informatics_classroom/classroom/routes.py
+++ informatics_classroom/classroom/routes.py
@@ -18,7 +18,6 @@ import requests
 # rbb setting for testing without authentication
 TESTING_MODE = Config.TESTING
 DATABASE = Config.DATABASE
-DATABASE = 'bids-class'
 
 ClassGroups=sorted(['PMAP','CDA','FHIR','OHDSI'])
 
